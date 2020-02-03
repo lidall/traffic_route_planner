ae6ee8f7166cbad008444f90b4fcba9dafddecba
lidall <32701176+lidall@users.noreply.github.com>
2020-02-03 14:13:34 +0100

    Update infoDisplayInstrumentedTest.java

--- app/src/androidTest/java/com/lida/traffic_planner/infoDisplayInstrumentedTest.java
+++ app/src/androidTest/java/com/lida/traffic_planner/infoDisplayInstrumentedTest.java
@@ -1,11 +1,8 @@
 package com.lida.traffic_planner;
 
-import android.widget.TextView;
 
-import com.google.android.gms.maps.MapView;
 import com.lida.APIclient.httpClient;
 
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,10 +14,10 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static org.hamcrest.CoreMatchers.instanceOf;
-import static org.hamcrest.core.IsNull.notNullValue;
-import static org.junit.Assert.assertThat;
+
 
 @RunWith(AndroidJUnit4.class)
 public class infoDisplayInstrumentedTest {
@@ -29,8 +26,9 @@ public class infoDisplayInstrumentedTest {
     public ActivityTestRule<MainActivity> activityRule
             = new ActivityTestRule<>(MainActivity.class);
 
-    @Before
-    public void init() throws Exception{
+
+    @Test
+    public void ensureListViewIsPresent() throws Exception {
         // Specify a valid string.
         MainActivity.token = httpClient.keyGet();
         onView(withId(R.id.originText))
@@ -39,22 +37,8 @@ public class infoDisplayInstrumentedTest {
                 .perform(typeText("Lindholmen"), closeSoftKeyboard());
         onView(withId(R.id.searchButton)).perform(click());
 
-    }
-
-
-    @Rule
-    public ActivityTestRule<routeDisplay> route  = new  ActivityTestRule<>(routeDisplay.class);
-
-    @Test
-    public void ensureListViewIsPresent() throws Exception {
-        routeDisplay routeActivity = route.getActivity();
-        TextView trafficInfo = routeActivity.findViewById(R.id.routeView);
-        assertThat(trafficInfo,notNullValue());
-        assertThat(trafficInfo, instanceOf(TextView.class));
-
-        MapView map = routeActivity.findViewById(R.id.mapView);
-        assertThat(map,notNullValue());
-        assertThat(map, instanceOf(MapView.class));
+        onView(withId(R.id.routeView)).check(matches(isDisplayed()));
+        onView(withId(R.id.mapView)).check(matches(isDisplayed()));
 
 
     }
